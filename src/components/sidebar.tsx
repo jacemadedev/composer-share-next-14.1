907a8b7f0ae0485d672b1560b0213b9831ab6062
Jace Made <wondrwhale@gmail.com>
2024-11-25 16:45:31 -0500

    Revert "Update sidebar.tsx"
    This reverts commit 07ab401c6484f209e59d5836bc4751b3e4cb5b97.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Home, BookOpen, MessageSquare, Clock, Settings, LogOut, CreditCard, ChevronRight, User, Menu, Crown } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuContent,
